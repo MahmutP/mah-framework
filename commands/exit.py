8b43fa63d868d9d2e7bce3a4510a46ab524ed7a7
Mahmut P. <mahcoder01@proton.me>
2025-07-27 21:50:47 +0300

    temel komut help ve exit kodlandı

---
+++ commands/exit.py
@@ -0,0 +1,18 @@
+# çıkış için temel komut
+import sys
+from typing import Any
+from core.command import Command
+from core.shared_state import shared_state 
+class Exit(Command):
+    Name = "exit"
+    Description = "Uygulamadan çıkar."
+    Category = "core"
+    Aliases = ["quit"]
+    def execute(self, *args: str, **kwargs: Any) -> bool:
+        print("Uygulamadan çıkış yapılıyor...")
+        if hasattr(shared_state, 'console_instance') and shared_state.console_instance:
+            shared_state.console_instance.shutdown()
+        else:
+            print("Console instance bulunamadı, doğrudan çıkılıyor.")
+            sys.exit(0) 
+        return True 

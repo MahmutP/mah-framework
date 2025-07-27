3bbf9fdcdc890f851631ce354bf02e210af03c41
Mahmut P. <mahcoder01@proton.me>
2025-07-27 22:23:28 +0300

    alias, back, banner temel komutları kodlandı

---
+++ commands/back.py
@@ -0,0 +1,18 @@
+# temel komut back, modül seçilmesi halinde geri root prompta dönmek için
+from typing import Any
+from core.command import Command
+from core.shared_state import shared_state
+class Back(Command):
+    Name = "back"
+    Description = "Mevcut modülden çıkarak ana kabuğa döner."
+    Category = "module"
+    Aliases = []
+    def execute(self, *args: str, **kwargs: Any) -> bool:
+        selected_module = shared_state.get_selected_module()
+        if selected_module:
+            shared_state.set_selected_module(None)
+            #(f"Modül '{selected_module.Name}' seçiminden çıkıldı. Ana kabuğa dönüldü.")
+            return True
+        else:
+            print("Zaten bir modül seçili değil. Ana kabuktasınız.")
+            return False

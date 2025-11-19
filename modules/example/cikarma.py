9d0c48946ae1823c5433dff8894c54e3d8ff9d8a
Mahmut Paşa <mahcoder01@proton.me>
2025-11-19 10:48:14 +0300

    yeni örnek modül çıkarma, çıkarma işlemi yapıyor.

---
+++ modules/example/cikarma.py
@@ -0,0 +1,36 @@
+from typing import Dict, Any
+from core.module import BaseModule
+from core.option import Option
+from rich import print
+
+class cikarma(BaseModule):
+    """Çıkarma işlemi yapmaya yarıyan örnek bir modül.
+
+    Args:
+        BaseModule (_type_): Ana komut sınıfı.
+    """
+    Name = "cikarma"
+    Description = "Çıkarma işlemi yapmaya yarıyan örnek bir modül."
+    Author = "Mahmut P."
+    Category = "example"
+    def __init__(self):
+        """init fonksiyon.
+        """
+        super().__init__()
+        self.Options = {
+            "first_number": Option("first_number", None, True, "Çıkarılacak birinci sayı."),
+            "second_number": Option("second_number", None, True, "Çıkarılacak ikinci sayı.")
+        }
+        for option_name, option_obj in self.Options.items():
+            setattr(self, option_name, option_obj.value)
+
+    def run(self, options: Dict[str, Any]):
+        """Modül çalışınca çalışacak fonksiyon
+
+        Args:
+            options (Dict[str, Any]): işlenecek Option'lar
+        """
+        print(f"Modül: '{self.Name}'")
+        print(f"Çıkarılacak birinci sayı: {options.get("first_number")}")
+        print(f"Çıkarılacak ikinci sayı: {options.get("second_number")}")
+        print(f"{options.get("first_number")} - {options.get("second_number")} = {int(options.get("first_number"))-int(options.get("second_number"))}")

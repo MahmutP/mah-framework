7c9547b1f34afa4160a0e7a2b05d73b1698ff36f
Mahmut Paşa <mahcoder01@proton.me>
2026-01-28 10:10:30 +0300

    usage ve examples eklendi

--- commands/alias.py
+++ commands/alias.py
@@ -21,13 +21,12 @@ class Alias(Command):
     Description = "Komutlar için kısayollar oluşturur, listeler ve siler."
     Category = "system"
     Aliases = []
-    Usage = "alias [kısayol] [hedef_komut] | alias -d <kısayol> | alias -l"
+    Usage = "alias <add|list|remove> [argümanlar]"
     Examples = [
-        "alias                    # Tüm alias'ları listeler",
-        "alias -l                 # Tüm alias'ları listeler",
-        "alias h help             # 'h' yazınca 'help' çalışır",
-        "alias s search           # 's' yazınca 'search' çalışır",
-        "alias -d h               # 'h' alias'ını siler"
+        "alias list               # Tüm alias'ları listeler",
+        "alias add h help         # 'h' yazınca 'help' çalışır",
+        "alias add s search       # 's' yazınca 'search' çalışır",
+        "alias remove h           # 'h' alias'ını siler"
     ]
     def __init__(self):
         """init fonksiyon

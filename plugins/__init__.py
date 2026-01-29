dc44484fa402fa6e5f82255ae0c551b88cbf866b
Mahmut Paşa <mahcoder01@proton.me>
2026-01-29 23:04:46 +0300

    mypy testlerinden geçmesi için eklendi

---
+++ plugins/__init__.py
@@ -0,0 +1,10 @@
+"""Plugin dosyalarının bulunduğu klasör.
+
+Bu klasör, Mah Framework için yazılmış plugin dosyalarını barındırır.
+Her plugin dosyası BasePlugin sınıfından türeyen bir sınıf içermelidir.
+
+Plugin'ler framework başlatıldığında otomatik olarak yüklenir ve
+belirlenen hook'lara göre tetiklenir.
+
+Örnek plugin yapısı için templates/plugin_template.py dosyasına bakınız.
+"""

